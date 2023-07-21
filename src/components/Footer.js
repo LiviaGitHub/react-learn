43f141dc0068a863ed6cd63b1c7909d006696973
LiviaGitHub <liviagithub@gmail.com>
2023-07-21 12:41:21 +0200

    .gitignore

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,3 @@
-
 import logo from '.././assets/images/logo192.png';
 import styles from '../UI/Footer.css';
 
@@ -6,7 +5,7 @@ const Footer = () => {
     return (
         <footer className={styles.footer}>
             <img src={logo} alt="Medal badge with a star" />
-        <p>See you</p>
+            <p>See you</p>
         </footer>
     );
 }
